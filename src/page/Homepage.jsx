feb468c3c72291c684e85e0f9f2cef49da389ab4
Pranesh Kaushik <rcvghahu@gmail.com>
2022-05-17 19:48:48 +0530

    Created Project of E-commerce ClothingApp and added Files for Homepage

---
+++ src/page/Homepage.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styles from "../styles.scss";
+const HomePage= () => (
+    <div className="homepage">
+        <div className="directory">
+            <div className="menu-items">
+                <div className="content">
+                    <div className="title">Hats</div>
+                    <span className="subtitle">Shop Now!</span>
+                </div>
+            </div>
+            <div className="menu-items">
+                <div className="content">
+                    <div className="title">Sneakers</div>
+                    <span className="subtitle">Shop Now!</span>
+                </div>
+            </div>
+            <div className="menu-items">
+                <div className="content">
+                    <div className="title">Jackets</div>
+                    <span className="subtitle">Shop Now!</span>
+                </div>
+            </div>
+            <div className="menu-items">
+                <div className="content">
+                    <div className="title">Men</div>
+                    <span className="subtitle">Shop Now!</span>
+                </div>
+            </div>
+            <div className="menu-items">
+                <div className="content">
+                    <div className="title">Women</div>
+                    <span className="subtitle">Shop Now!</span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default HomePage;
